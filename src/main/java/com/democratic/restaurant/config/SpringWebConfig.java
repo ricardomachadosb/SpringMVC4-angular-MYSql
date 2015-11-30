080eb8d42e399f2726b4a204680089d35585c0f6
unknown <ricardomachado.sb@gmail.com>
2015-11-30 18:33:59 -0200

    Adding database configuration

--- src/main/java/com/democratic/restaurant/config/SpringWebConfig.java
+++ src/main/java/com/democratic/restaurant/config/SpringWebConfig.java
@@ -1,8 +1,11 @@
 package com.democratic.restaurant.config;
  
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -30,6 +33,21 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setPrefix("/WEB-INF/views/jsp/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
+	}
+	
+	/**
+	 * @return
+	 */
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/democratic_restaurant");
+		dataSource.setUsername("root");
+		dataSource.setPassword("1234");
+
+		return dataSource;
 	}
  
 }
--- src/main/java/com/democratic/restaurant/model/Restaurant.java
+++ src/main/java/com/democratic/restaurant/model/Restaurant.java
@@ -1,13 +1,26 @@
 package com.democratic.restaurant.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author Ricardo Machado
  *
  */
+@Entity
+@Table(name="restaurant")
 public class Restaurant {
 	
+	@Column(name="name")
 	private String name;
 	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
 	public Restaurant() {}
